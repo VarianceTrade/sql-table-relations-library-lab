8139f69334102d3763506162226e39809007f9e9
Brandon <sugb73@gmail.com>
2021-03-21 13:30:21 -0400

    commit tree 1113dc4799efdfec2a4d4a1fb57d628c7a077fdf

---
+++ lib/insert.sql
@@ -0,0 +1,79 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("series title 1", 1, 1);
+
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("series title 2", 2, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("book title 1", 2000, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ("book title 2", 2001, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("book title 3", 2002, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("book title 4", 2004, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ("book title 5", 2006, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ("book title 6", 2010, 2);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 1", "species 1", "motto 1", 1);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 2", "species 2", "motto 2", 2);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 3", "species 3", "motto 3", 1);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 4", "species 4", "motto 4", 2);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 5", "species 5", "motto 5", 1);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 6", "species 6", "motto 6", 2);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 7", "species 7", "motto 7", 2);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("character name 8", "species 8", "motto 8", 1);
+
+INSERT INTO subgenres (name) VALUES ("subgenre 1");
+
+INSERT INTO subgenres (name) VALUES ("subgenre 2");
+
+INSERT INTO authors (name) VALUES ("author 1");
+
+INSERT INTO authors (name) VALUES ("author 2");
+
+INSERT INTO character_books (book_id, character_id) VALUES (1, 1);
+
+INSERT INTO character_books (book_id, character_id) VALUES (2, 2);
+
+INSERT INTO character_books (book_id, character_id) VALUES (3, 3);
+
+INSERT INTO character_books (book_id, character_id) VALUES (4, 4);
+
+INSERT INTO character_books (book_id, character_id) VALUES (5, 5);
+
+INSERT INTO character_books (book_id, character_id) VALUES (6, 6);
+
+INSERT INTO character_books (book_id, character_id) VALUES (7, 7);
+
+INSERT INTO character_books (book_id, character_id) VALUES (8, 8);
+
+INSERT INTO character_books (book_id, character_id) VALUES (1, 1);
+
+INSERT INTO character_books (book_id, character_id) VALUES (2, 2);
+
+INSERT INTO character_books (book_id, character_id) VALUES (3, 3);
+
+INSERT INTO character_books (book_id, character_id) VALUES (4, 4);
+
+INSERT INTO character_books (book_id, character_id) VALUES (5, 5);
+
+INSERT INTO character_books (book_id, character_id) VALUES (6, 6);
+
+INSERT INTO character_books (book_id, character_id) VALUES (7, 7);
+
+INSERT INTO character_books (book_id, character_id) VALUES (8, 8);
+
+
+
+
+
+
+
+
